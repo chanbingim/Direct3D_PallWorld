17805cd0656dfb45bb994f069a2275a2bf616ba5
Chanbin Kim <chanbin@example.com>
2025-08-11 08:45:39 +0900

    Update_08_11
    1.텍스처 버그 수정

--- EngineSDK/Inc/Engine_Struct.h
+++ EngineSDK/Inc/Engine_Struct.h
@@ -45,7 +45,7 @@ namespace Engine
 		_float3		vTangent;
 		_float2		vTexcoord;
 
-		static constexpr unsigned int			  iNumElements = { 3 };
+		static constexpr unsigned int			  iNumElements = { 4 };
 		static constexpr D3D11_INPUT_ELEMENT_DESC VertexDesc[] = {
 			{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
 			{"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0},
